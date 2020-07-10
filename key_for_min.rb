0b74e5c15a357f37eacf29a2042f12ef0db9077e
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-10 16:29:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,8 @@
 def key_for_min_value(name_hash)
   min_key = nil
   #This can be assigned nil, since it's value is soley dependent on min_value
-  min_value = "The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. beep A single lap should be completed each time you hear this sound. ding Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start."
-  #This string is ridiculously long, so that the first value passed is almost guaranteed to be less than it.
-  #If for some reason I was working with strings that might be longer, I could substitute in the longest english word.(It's multiple pages long, using size 12 font)
+  min_value = (2**(0.size * 8 -2) -1) 
+  #This is the maximum integer storable, according to stack overflow. This way the first value passed is less than it.
   name_hash.each do |key, value|
     if value < min_value
       min_value = value
